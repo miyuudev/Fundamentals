8ddd97fa4390a3851d9bf2d4dbd9e4602dfff8d6
Miyuki <miyuu.dev@gmail.com>
2025-11-01 14:11:18 +0900

    First commit

---
+++ myNumber.py
@@ -0,0 +1,13 @@
+"""
+1. Validador de CPF Japonês (MyNumber simplificado)
+
+Crie um programa que receba uma string representando um número de identificação (12 dígitos) e:
+
+Verifique se contém apenas números.
+
+Valide o comprimento (12 dígitos).
+
+Retorne “Válido” ou “Inválido”.
+
+💡 Desafio extra: implemente uma função que gera um número aleatório válido.
+"""
